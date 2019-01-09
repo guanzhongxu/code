baef4a744b9a616cfff09bab8a5432d401886990
guanzhongxu <46061268+guanzhongxu@users.noreply.github.com>
2019-01-10 01:34:24 +0800

    Add files via upload

---
+++ getElements.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @param id
+ * @returns {HTMLElement}
+ * @author 管忠旭
+ */
+function $MyId(id){
+    return document.getElementById(id);
+}
+/**
+ *
+ * @param class
+ * @returns {HTMLElement}
+ * @author 管忠旭
+ */
+function $MyClass(Class){
+    return document.getElementsByClassName(Class);
+}
+/**
+ *
+ * @param label
+ * @returns {HTMLElement}
+ * @author 管忠旭
+ */
+function $MyTag(Tag){
+    return  document.getElementsByTagName(Tag);
+}
